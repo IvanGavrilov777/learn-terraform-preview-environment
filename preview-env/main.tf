4ba51c32db2b82ef7b8f57a12c43fd93e13da708
Ivan Gavrilov <gavrilov.ivan@yahoo.com>
2023-05-06 01:26:34 +0300

    update vercel proj name and TFC ORG

--- preview-env/main.tf
+++ preview-env/main.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "shared" {
   backend = "remote"
 
   config = {
-    organization = "hashicorp-training"
+    organization = "ivan-testing"
     workspaces = {
       name = "hcup-be-shared"
     }
